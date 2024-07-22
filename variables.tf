011097bb166e34cc5bd7da47fa137d7c841c9610
Dmitrij Lejkin <dmitrij.lejkin@abraxas.ch>
2024-07-22 06:42:18 +0200

    chore: Update merge request template with description and checklist

--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable "application_cmd_admin_name" {
   description = "The name of the application"
   type        = string
-  
+
 }
 
 variable "application_cmd_published_name" {
   description = "The name of the application"
   type        = string
-  
+
 }
 
 variable "application_cmd_command_line_executable" {
   description = "The command line executable"
   type        = string
-  
+
 }
 
 variable "application_cmd_working_directory" {
   description = "The working directory"
   type        = string
-  
+
 }
 
 variable "deliverygroup_name" {
   description = "Delivery group"
   type        = set(string)
-  
+
 }
 
 variable "description_pubblished_apps" {
   description = "For the description"
   type        = string
-  
+
 }
 variable "visibility_user" {
   description = "The visibility of the application"
   type        = set(string)
-  
+
 }
 
 
 variable "application_icon" {
   type        = string
   description = "The icon of the application"
-  
+
 
 }
 
